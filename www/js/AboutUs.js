5903de5d4a36edf70c26853b4e02a24408d22a44
Magnus <isthisthereallife@riseup.net>
2019-11-25 15:08:55 +0100

    added: lite text i omOss

--- www/js/AboutUs.js
+++ www/js/AboutUs.js
@@ -7,13 +7,29 @@ class AboutUs {
 
   render() {
     $('main').html(/*html*/`
-      <section class="row">
-        <div class="col">
-          <h1>Om oss</h1>
-          <p>Vi är litet företag som säljer ekologiska badprodukter.</p>
-        </div>
+      <section class="container data-aboutUs">
+        <section class="row d-flex">
+          <div class="col col-12 col-md-6">
+            <h2 class="h1">About us</h2>
+            <p>We love snakes. All the snakes.</p>
+            <p>Green snakes, yellow snakes, red snakes. </p>
+            <p>You name it.</p>
+            <p>We love it</p>
+          </div>
+          <div class="col col-12 col-md-6">
+          <h2> Our favourite snakes:</h2>
+          <ul>
+            <li>Huggorm</li>
+            <li>Anaconda</li>
+            <li>Kobra</li>
+            </ul>
+          </section>
+        <section class="row d-flex">
+          <section class="col">
+          </section>
+        </section>
       </section>
-    `);
+      `);
   }
 
 }
