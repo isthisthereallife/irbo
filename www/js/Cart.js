142b59a9aedd104ffd7ec5f0484cc1ff726e259d
Magnus <isthisthereallife@riseup.net>
2019-11-26 10:48:45 +0100

    klicka för att lägga till i kundvagn
    skapat en array sparad till localstorage

--- www/js/Cart.js
+++ www/js/Cart.js
@@ -1,13 +1,21 @@
 class Cart {
 
-  /*
-    
-    I am a Cart!
 
-    I want to be a shopping-cart!
-  */
+  constructor() {
+    //skapa en array i localstorage
+    store.cartProducts = store.cartProducts || []
+    store.save();
+  }
 
   add(product) {
+
+    //lägg till produkten som valts till arrayen i localstorage
+    store.cartProducts.push(product)
+    
+    store.save();
+
+
+
     // We are doing a json stringify of the product
     // minus the cart property of a product
     // (which is just a reference to the cart)
@@ -15,26 +23,27 @@ class Cart {
     // We don't need a JSON.stringify when we have
     // intelligent methods... This i purely to
     // show what product that is intended to be added...
-    alert(`
-      I am a cart. 
-      I have no render-method and no methods that calc sums.
-      I have no add and remove methods...
-      But I know that you tried to add this product to me:
-      ${JSON.stringify({ ...product, cart: undefined }, '', '  ')}
-      // remove all extra spaces after a new-line
-    `.replace(/\n\s*/g, '\n'))
+
+    // alert(`
+    //   I am a cart. 
+    //   I have no render-method and no methods that calc sums.
+    //   I have no add and remove methods...
+    //   But I know that you tried to add this product to me:
+    //   ${JSON.stringify({ ...product, cart: undefined }, '', '  ')}
+    //   // remove all extra spaces after a new-line
+    // `.replace(/\n\s*/g, '\n'))
   }
-  
-  render(){
+
+  render() {
+
     $('main').html(`
   <section class="row">
     <div class="col">
     <h1>Cart</h1>
     <p> här ska vi lägga in en lista av valda produkter, t ex 
-
     </div>
     </section>
     `);
-}
+  }
 
 }
