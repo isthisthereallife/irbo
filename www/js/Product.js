b8c2c4dacebca93875de2f880bacf67f9a60231e
Omar <Omanas_98@hotmail.com>
2019-11-27 10:51:01 +0100

    Bort kommenterade kod för JS-animering för kundvagnen,

--- www/js/Product.js
+++ www/js/Product.js
@@ -44,27 +44,28 @@ class Product {
     // This is how I render myself on a product-detail page
     // there it only me
     $('main').html(/*html*/`
-     <section class="row">
-        <div class="col">
-          <h1>${this.name}</h1>
-        </div>
-      </section>
-      <section class="row">
-        <div class="col-12 col-lg-8">
-          <p>${this.description}</p>
-          <h5 class="pb-2">About this Snake</h5>
-          <div class="row pb-4">
-              <h6 class="col-3">Costs ${this.price}kr/kg</h6>
-              <h6 class="col-3">Weights ${this.weight}kg</h6>
-              <h6 class="col-3">Shipping price at ${this.weight * 40}kr</h6>
-            </div>
-          <button id="buy-button-${this.id}" class="btn btn-primary my-2 ">Köp</button>
-          </div>
-          <p></p>
-        <div class="col-12 col-lg-4">
-          <img class="img-fluid border border-primary rounded" src="${this.image}">
-        </div>
-      </section>
+    <section class="row">
+    <div class="col">
+      <h1>${this.name}</h1>
+    </div>
+  </section>
+  <section class="row item">
+    <div class="col-12 col-lg-8 item">
+      <p>${this.description}</p>
+			<span><i class="product-animation"></i></span>
+      <h5 class="pb-2">About this Snake</h5>
+      <div class="row pb-4">
+        <h6 class="col-3">Costs ${this.price}kr/kg</h6>
+        <h6 class="col-3">Weights ${this.weight}kg</h6>
+        <h6 class="col-3">Shipping price at ${this.weight * 40}kr</h6>
+      </div>
+      <button id="buy-button-${this.id}" class="btn btn-primary my-2 ">Köp</button>
+    </div>
+    <p></p>
+    <div class="col-12 col-lg-4">
+      <img class="img-fluid border border-primary rounded" src="${this.image}">
+    </div>
+  </section>
     `);
   }
 
@@ -74,12 +75,14 @@ class Product {
     return `
       <div class="col-12 col-md-6 col-lg-4 mt-5">
         <a href="#${this.slug}">
-        <div class="row">
-           <h4 class="-100" class="col-6">${this.name}</h4>
-          </div>
-          <img class="img-fluid border border-primary rounded w-70 h-70" src="${this.image}">
-          <button id="buy-button-${this.id}" class="btn btn-primary my-2">Köp</button>
-        </a>
+        <!--<span><i class="product-animation"></i></span>-->
+        <div class="clear"></div>
+        <div class="row items">
+            <h4 class="-100" class="col-6">${this.name}</h4>
+            </div>
+            <img class="img-fluid border border-primary rounded w-70 h-70" src="${this.image}">
+            <button id="buy-button-${this.id}" class="btn btn-primary my-2">Köp</button>
+          </a>
       </div>
     `
   }
