eec5b49655d44d6d5a5653198f44150f65865268
Omar <Omanas_98@hotmail.com>
2019-11-27 15:17:02 +0100

    Design hantering, bilder och text smat köp knapp

--- www/js/Product.js
+++ www/js/Product.js
@@ -1,6 +1,4 @@
 class Product {
-
-
   /*
     I am a Product.
 
@@ -24,9 +22,9 @@ class Product {
     // this a delegated event handler:
     // * when you click on the body
     // * if you clicked inside something matching the css-selector
-    //   #buy-button-myId 
+    //   #buy-button-myId
     // * then run the anonymous arrow function...
-    $('body').on('click', `#buy-button-${this.id}`, e => {
+    $("body").on("click", `#buy-button-${this.id}`, e => {
       // e is the event object
       // it has a preventDefault method
       // when you call that method it prevents the browser
@@ -43,25 +41,27 @@ class Product {
   render() {
     // This is how I render myself on a product-detail page
     // there it only me
-    $('main').html(/*html*/`
+    $("main").html(/*html*/ `
      <section class="row">
         <div class="col">
           <h2 class="h1">${this.name}</h2>
         </div>
       </section>
       <section class="row">
-        <div class="col-12 col-lg-8">
+        <div class="col-12 col-lg-6">
           <p>${this.description}</p>
           <h5 class="pb-2">Orminformation</h5>
           <div class="row pb-4">
               <h6 class="col-3">Pris ${this.price} kr</h6>
               <h6 class="col-3">Vikt ${this.weight} kg</h6>
               <h6 class="col-3">Fraktpris ${this.weight * 40} kr</h6>
+              <button id="buy-button-${this.id}" class="btn btn-primary my-2 col-3">Köp</button>
           </div>
-          <button id="buy-button-${this.id}" class="btn btn-primary my-2 ">Köp</button>
         </div>
-        <div class="col-12 col-lg-4">
-          <img class="img-fluid border border-primary rounded" src="${this.image}">
+        <div class="col-12 col-lg-6">
+          <img class="img-fluid border border-primary rounded" src="${
+      this.image
+      }">
         </div>
       </section>
     `);
@@ -71,18 +71,21 @@ class Product {
     // This is how I render myself in a list of products
     // (this method is called from a ProductList)
     return `
-      <div class="col-12 col-md-6 col-lg-4 mt-5 w-100">
+      <div class="col-12 col-md-6 col-lg-4 mt-3">
         <a href="#${this.slug}">
-          <div class="row">
-            <h4 class="-100" class="col-6 text-center">${this.name}</h4>
+        <img class="img-fluid border border-primary rounded w-70 h-70" src="${this.image}">
+          <div class="row mb-3 ml-1">
+              <button id="buy-button-${this.id}" class="col-4 btn btn-primary mt-2">Köp</button>
+              <div class="col-8">
+                <div class="row">
+                  <h4 class="-100 col">${this.name}</h4>
+                  <h6 class="col-12">${this.price}:-</h6>
+                </div>
+              </div>
           </div>
-          <img class="img-fluid border border-primary rounded w-70 h-70" src="${this.image}">
+          </div>
         </a>
-        <div>
-        <button id="buy-button-${this.id}" class="btn btn-primary my-2">Köp</button>
-        </div>
       </div>
-    `
+    `;
   }
-
 }
