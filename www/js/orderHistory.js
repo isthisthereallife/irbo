4055e0f33aaea5819237b10db80ee9a5138f7d98
Omar <Omanas_98@hotmail.com>
2019-12-04 13:45:04 +0100

    updatering på about us layout, bild gallerin

---
+++ www/js/orderHistory.js
@@ -0,0 +1,19 @@
+class orderHistory{
+    constructor(){
+        
+    }
+
+    render(){ 
+        $("main").html(`
+            <section class="container">
+                <h1 class="text-center">Din order historik</h1>
+                <h4 class="float-right">Sortera efter datum <button class=" btn-primary btn">senaste</button>
+                </h4>
+                <section class="row">
+                    <img class="col-6" src="${Product.image}" alt="${Product.name}">
+                    <span class="col-6"> ${Product.price} Kr/st</span> 
+                </section>
+            </section>
+        `);
+    }
+};
