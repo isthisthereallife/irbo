03d6912715ce010687cf6aa789149543b2c3bd1b
Magnus <isthisthereallife@riseup.net>
2019-12-10 17:02:21 +0100

    added: ett villkor, så att klicket endast sker om dropdown visas

--- www/js/NavAnimation.js
+++ www/js/NavAnimation.js
@@ -3,25 +3,27 @@ class NavAnimation {
   constructor() {
     $('nav').append('<div class="underline d-none d-lg-block"/>');
     $('nav .navbar-link').on('click', e => this.underlineSlider(e));
-    
+
     // fejka ett klick på hamburgaren efter val från dropdown-naven
     $('#navbarSupportedContent').on('click', '.navbar-link', function () {
-      $('#data-burgarn').click(); 
+      if ($('#navbarSupportedContent').hasClass('show')) {
+        $('#data-burgarn').click();
+      }
     })
 
   }
 
-  
-  
-  
+
+
+
   underlineSlider(e) {
     let clickedLink = $(e.target);
     let left = $(clickedLink).offset().left + 8;
     let width = $(clickedLink).width();
     let underline = $('.underline');
-    
-  
-  
+
+
+
     underline.animate(
       {
         left: left,
@@ -29,12 +31,12 @@ class NavAnimation {
       },
       500
     );
-    
+
   }
-   
+
 }
 
-  
+
 
 
 
