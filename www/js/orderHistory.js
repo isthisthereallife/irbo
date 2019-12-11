58588d0f2e26cc3ec0387f1aedc28da0440bbd44
Magnus <isthisthereallife@riseup.net>
2019-12-11 14:53:11 +0100

    Crewprogramming livecoding orderhistory sortering

--- www/js/orderHistory.js
+++ www/js/orderHistory.js
@@ -1,43 +1,71 @@
-class OrderHistory{
+class OrderHistory {
 
-    constructor(){
-    $('body').on('click', '.confirm-buy-btn', e =>{
-        this.emptyCart()
+  constructor() {
+    $('body').on('click', '.confirm-buy-btn', e => {
+      this.emptyCart()
     })
-    }
-    
-            
-    render(){ 
-       
-        
-        $("main").html(`
-            <section class="container">
-                <h1 class="text-center">Din order historik</h1>
-                <h4 class="float-right">Sortera efter datum <button class=" btn-primary btn">senaste</button>
-                </h4>
-                <section class="row">`)
-                
-                store.order.forEach(lillaRender)
-                function lillaRender(item, index){
-                    console.log(item.surname," index: ",index)
-                    $("main").append(/*html*/`
-                    <p>Beställningsdatum: ${item.date}</p>
-                    <p>Beställare: ${item.surname}</p>
-                    `)
-                    for(let i = 0; i < item.order.length; i++){
-                    $("main").append(/*html*/`<p>${item.order[i].name}, ${item.order[i].qty} st</p>
-                    `)
-                    }
-                    $("main").append(/*html*/`<p>Totalpris: ${item.price} kr</p>`)
-                }
-    }
+    $('body').on('click', '#sort-btn', e => {
+      console.log(JSON.stringify(store.order))
 
-    emptyCart(){
-        store.cartProducts = []
-        store.cartQty = 0
-        $('.oi-cart').html(" " + store.cartQty)
-        console.log("Emptied store.cartProducts")
+      store.order.reverse();
+      console.log(store.order)
+      this.render()
+    })
+  }
+
+
+  render() {
+
+
+    $("main").html(/*html*/`
+            <section class="container">
+              <div class="row">
+                <div class="col">
+                  <h1 class="text-center mt-5">Tack för att du handlar hos oss!</h1>
+                <div class="row">
+                  <div class="col-8">
+                    <h3 class="text-center mt-5 ">Din orderhistorik</h3></div>
+                  <div class="col-4">
+                    <button type="button" class="btn btn-primary" id="sort-btn">Sortera</button></div>
+                  <div>
+                <div class="row">
+                <div class="col-10">
+                  
+               
+                  <ul>
+                 
+              `)
+
+
+    store.order.forEach(lillaRender)
+    function lillaRender(item, index) {
+      console.log(item.surname, " index: ", index)
+
+      $('main ul').append(/*html*/`
+      <li class="list-unstyled shadow p-2 mb-2 bg-white rounded data-list-item">
+        <p>Beställningsdatum: ${item.date}</p>
+        <p>Beställare: ${item.surname}</p>
+        `)
+
+      for (let i = 0; i < item.order.length; i++) {
+        $('main li').append(/*html*/`<p>${item.order[i].name}, ${item.order[i].qty} st</p>
+                    `)
+      }
+      $('main li').append(/*html*/`<p>Totalpris: ${item.price} kr</p>
+      </li></ul>
+      `)
     }
-    
-    
+      $('main').append(/*html*/`<span><a class="nav-link" href="#produkter"><button type="button" class="btn btn-light btn-lg productpage-btn order-sm-1 order-md-2">Till butiken</button></a>
+      </span>`)
+  }
+
+  emptyCart() {
+    store.cartProducts = []
+    store.cartQty = 0
+    store.save()
+    $('.oi-cart').html(" " + store.cartQty)
+    console.log("Emptied store.cartProducts")
+  }
+
+
 };
