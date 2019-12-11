b12799c8d4b6733322821441030e5ba764dc56b7
Michelle Opey <mipanowicz@gmail.com>
2019-12-11 16:11:33 +0100

    tiden avrundad o svensk

--- www/js/Orderpage.js
+++ www/js/Orderpage.js
@@ -2,7 +2,6 @@ class Orderpage{
 
     constructor(){
         console.log('hallå')
-        //store.order = [];
         
 $('body').on('click', '.yes-btn', function () { 
   $('body').removeClass('modal-open');
@@ -14,10 +13,11 @@ $('body').on('click', '.yes-btn', function () {
   email: $('#email').val(),
   adress: $('#address').val(),
   city: $('#city').val(),
-  date: new Date(),
+  date: new Date().toLocaleString('SV'),
   order: store.cartProducts,
   price: store.grandTotalSum
 });
+
 
 //store.order.push(info)
 
@@ -80,7 +80,7 @@ render() {
 
 <div class="modal fade" id="confirmationModal" tabindex="-1" role="dialog" aria-labelledby="confirmationModalLabel" aria-hidden="true" data-backdrop="false">
   <div class="modal-dialog" role="document">
-    <div class="modal-content">
+    <div class="modal-content modal-cool-color">
       <div class="modal-header">
         <h5 class="modal-title" id="confirmationModalLabel">Varukorg</h5>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
