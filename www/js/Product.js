9293dc36e0c0834e4f7c96ebfdf49668304bf954
Omar <Omanas_98@hotmail.com>
2019-12-13 09:02:36 +0100

    format av "orminformation" på produktspec sida är fixat

--- www/js/Product.js
+++ www/js/Product.js
@@ -70,24 +70,24 @@ class Product {
     $("main").html(/*html*/ `
     <section class="container">
      <section class="row">
-        <div class="col ">
+        <section class="col ">
           <h2 class="h1">${this.name}</h2>
-        </div>
+        </section>
       </section>
       <section class="row product">
-        <div class="col-12 col-lg-6">
+        <section class="col-12 col-lg-6">
           <p>${this.description}</p>
-          <h5 class="pb-2">Orminformation</h5>
-          <div class="row pb-4">
-              <h6 class="col-3">Pris ${this.price} kr</h6>
-              <h6 class="col-3">Vikt ${this.weight} kg</h6>
-              <h6 class="col-3">Fraktpris ${this.weight * 40} kr</h6>
-              <button id="buy-button-${this.id}" class="btn btn-primary my-2 col-3">Köp</button>
-          </div>
-        </div>
-        <div class="col-12 col-lg-6">
+          <h4 class="pb-2">Orminformation</h4>
+          <section class="row pb-4 align-items-center">
+              <h6 class="col-7">Pris ${this.price} kr</h6>
+              <h6 class="col-5">Vikt ${this.weight} kg</h6>
+              <h6 class="col-7">Fraktpris ${this.weight * 40} kr</h6>
+              <button id="buy-button-${this.id}" class="buy-button ml-3 btn btn-primary my-2 col-5">Köp</button>
+          </section>
+        </section>
+        <section class="col-12 col-lg-6">
           <img class="img-fluid border border-primary rounded" src="${this.image}">
-        </div>
+        </section>
       </section>
       </section>
     `);
@@ -97,21 +97,21 @@ class Product {
     // This is how I render myself in a list of products
     // (this method is called from a ProductList)
     return `
-        <div class="col-12 col-md-6 col-lg-4 mt-3 product">
+        <section class="col-12 col-md-6 col-lg-4 mt-3 product">
           <a href="#${this.slug}">
           <img class="img-fluid border border-primary rounded w-70 h-70" src="${this.image}">
-            <div class="row mb-3 ml-1 mt-2">
+            <section class="row mb-3 ml-1 mt-2">
                 <button id="buy-button-${this.id}" class="col-4 btn btn-primary mt-2 buy-btn">Köp</button>
-                <div class="col-8">
-                  <div class="row">
+                <section class="col-8">
+                  <section class="row">
                     <h4 class="-100 col produktListaRubrik">${this.name}</h4>
                     <h6 class="col-12">${this.price}:-</h6>
-                  </div>
-                </div>
-            </div>
-            </div>
+                  </section>
+                </section>
+            </section>
+            </section>
           </a>
-        </div>
+        </section>
     `;
   }
 }
